1173e895b054b0190a4893b8e0e9340ac887f4fa
tomtom <tomasz.les@pw.edu.pl>
2021-12-22 09:00:02 +0100

    Add project files.

---
+++ P01ZadanieTemperatury/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01ZadanieTemperatury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransformatorJednostek tj = new TransformatorJednostek();
+
+            double temp = 20;
+
+            double wynik= tj.ZamienTemperature(temp,'p','f');
+
+
+            TransformatorJednostek2 tj2 = new TransformatorJednostek2();
+           
+            double temp2= tj2.ZamienTemperatureNaInna(20, Jednostka.Celsjusz, Jednostka.Fahrenheit);
+
+            temp2= tj2.ZamienTemperatureNaInna(10, Jednostka.Celsjusz, Jednostka.Fahrenheit);
+
+
+            TransformatorJednostek3 tj3 = new TransformatorJednostek3();
+            tj3.jednWejsciowa = Jednostka.Fahrenheit;
+            tj3.jednDocelowa = Jednostka.Kelvin;
+
+            double wynik3= tj3.ZamienTemperatureNaInna(10);
+
+            tj3.jednWejsciowa = Jednostka.Celsjusz;
+            tj3.jednDocelowa = Jednostka.Kelvin;
+
+            double wynik4 = tj3.ZamienTemperatureNaInna(20);
+
+
+            Console.WriteLine(wynik);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P01ZadanieTemperatury/TransformatorJednostek.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01ZadanieTemperatury
+{
+
+
+    class TransformatorJednostek
+    {
+        /// <summary>
+        /// To jest metoda, która zamienia temperaturę z jdnej jednostki na drugą
+        /// </summary>
+        /// <param name="n">Dana wejściowa (temperatura)</param>
+        /// <param name="jednWejsciowa">Jednostka wejściowa z zakresu c,f,k</param>
+        /// <param name="jednDocelowa">Jednostka docelowa z zakresu c,f,k</param>
+        /// <returns>Zwraca przekonwertowaną temperaturę</returns>
+        public double ZamienTemperature(double n, char jednWejsciowa, char jednDocelowa)
+        {
+            if (jednWejsciowa == 'c' && jednDocelowa == 'f')
+                return celcjuszNaFahrenheit(n);
+
+            if (jednWejsciowa == 'c' && jednDocelowa == 'k')
+                return celcjuszNaKelwin(n);
+
+            if (jednWejsciowa == 'f' && jednDocelowa == 'k')
+                return fahrenheitNaKelwin(n);
+
+            if (jednWejsciowa == 'f' && jednDocelowa == 'c')
+                return fahrenheitNaCelcjusz(n);
+
+            if (jednWejsciowa == 'k' && jednDocelowa == 'c')
+                return kelwinNaCelsjusz(n);
+
+            if (jednWejsciowa == 'k' && jednDocelowa == 'f')
+                return kelwienNaFahrenheit(n);
+
+            throw new Exception("Nieobsługiwana jednostka");
+        }
+
+        private double celcjuszNaFahrenheit(double n)
+        {
+            return n * 1.8 + 32;
+        }
+
+        private double celcjuszNaKelwin(double n)
+        {
+            return n +273.15;
+        }
+
+        private double fahrenheitNaKelwin(double n)
+        {
+            return (n - 32) / 1.8 + 273.15;
+        }
+
+        private double fahrenheitNaCelcjusz(double n)
+        {
+            return (n - 32)/1.8;
+        }
+
+        private double kelwinNaCelsjusz(double n)
+        {
+            return n-273.15;
+        }
+
+        private double kelwienNaFahrenheit(double n)
+        {
+            return (n-273.15)*1.8 + 32;
+        }
+
+    }
+}
